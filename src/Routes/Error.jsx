0a24aa5f8fc6089685875a2675ca06fb9c6e9a90
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-03-19 11:41:02 -0500

    404 page title added

--- src/Routes/Error.jsx
+++ src/Routes/Error.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { TabTitle } from '../Utilities/TabTitle'
 import "../Styles/Global.scss"
 
 import Hero from '../Sections/Hero'
 import ErrorLabel from '../Components/ErrorLabel'
 
 const Error = () => {
+  TabTitle("D&E | 404")
   return (
     <div className={"Page"}>
         <Hero />
