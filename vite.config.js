6752123fea2c411969901828b697565a13361b0d
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-03-24 17:42:03 -0500

    Config file experiment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+  css: {
+    postcss,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  server: {
+    host: true,
+  },
+});
